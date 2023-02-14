9cd4c11f3b0f71ccdb2606a34df0d682ba8dd374
Xavier <xaviramos668@gmail.com>
2023-02-14 20:35:53 -0300

    Pagina

---
+++ js/script-text.js
@@ -0,0 +1,14 @@
+const letra =document.getElementById("letra");
+const fuentes=["Anson-R3","GENUINE","CROOKED-Bold"];
+var index=0;
+function cambioFuente ()
+{
+    letra.style.fontFamily=fuentes[index];
+    index=index+1
+    if(index==3)
+    {
+        index=0
+    }
+}
+setInterval(cambioFuente,2000);
+
